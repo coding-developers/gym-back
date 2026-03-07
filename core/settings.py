8f61ef14b8dec6474767b080849a053ab8f82c02
Lisboa <pedrolisboadeveloper@gmail.com>
2026-03-07 05:42:05 -0300

    update settings

--- core/settings.py
+++ core/settings.py
@@ -26,13 +26,10 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-<<<<<<< HEAD
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
-=======
+
 ALLOWED_HOSTS = [
     h.strip() for h in config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 ]
->>>>>>> origin/dev
 
 
 # Application definition
