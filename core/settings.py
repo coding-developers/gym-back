452d9f7b3b81817d10e9db7067290b1f64fda197
Lisboa <pedrolisboadeveloper@gmail.com>
2026-03-07 04:46:17 -0300

    feat

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 
 
 # Application definition
