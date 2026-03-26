6d4eb21d616d141167526c582c1e7674baeb7930
Lisboa <pedrolisboadeveloper@gmail.com>
2026-03-26 13:37:18 -0300

    refix: payments

--- financial/serializers.py
+++ financial/serializers.py
@@ -3,10 +3,21 @@ from .models import Payment, ProductTransaction
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Payment
         fields = "__all__"
         read_only_fields = ("created_at", "updated_at", "paid_at")
+
+    def get_user(self, obj):
+        from gym.models import User
+        from gym.serializers import UserSerializer
+        try:
+            user = User.objects.get(pk=obj.user_id)
+            return UserSerializer(user).data
+        except User.DoesNotExist:
+            return None
 
 
 class ProductTransactionSerializer(serializers.ModelSerializer):
